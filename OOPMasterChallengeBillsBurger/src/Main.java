9117df28d9f447c141ce04e8bacec34cb5b3dace
nfillip <nick.m.fillip@gmail.com>
2024-01-05 14:55:08 -0700

    OOP Final Challenge

--- OOPMasterChallengeBillsBurger/src/Main.java
+++ OOPMasterChallengeBillsBurger/src/Main.java
@@ -19,10 +19,17 @@ public class Main {
 //        regularMeal.setDrinkSize("LARGE");
 //        regularMeal.printItemizedList();
 
-        MealOrder secondMeal = new MealOrder("turkey", "7-up",
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up",
+//                "chili");
+//        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up",
                 "chili");
-        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
-        secondMeal.setDrinkSize("SMALL");
-        secondMeal.printItemizedList();
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE",
+                "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 }
